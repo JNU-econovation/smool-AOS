9bd3af34175f2f7c46acb1fd97d613a7a8e593d3
seoyun <xeonj@naver.com>
2024-01-06 16:48:08 +0900

    feat: 설정 화면 추가

---
+++ app/src/main/java/com/example/moodtraker/screen/SettingScreen.kt
@@ -0,0 +1,34 @@
+package com.example.moodtraker.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun SettingScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF433E76),
+                        Color(0xFF9394BB)
+                    )
+                )
+            )
+    ) {
+        Text(
+            text = "설정 화면",
+            fontSize = 30.sp
+        )
+    }
+
+}
