a9668e356dd5f6f3c4edf03e200c33f6b960e970
seoyun <xeonj@naver.com>
2024-01-09 00:59:37 +0900

    feat: 로그인 기능 추가

---
+++ app/src/main/java/com/example/moodtraker/LoginActivity.kt
@@ -0,0 +1,309 @@
+package com.example.moodtraker
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import com.example.moodtraker.ui.theme.MoodtrakerTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent{
+            LoginScreen()
+        }
+    }
+}
+
+
+@Composable
+fun LoginScreen(){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF433E76),
+                        Color(0xFF9394BB)
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+
+        var credentials by remember { mutableStateOf(Credentials()) }
+        var context = LocalContext.current
+
+        Box(
+
+        ) {
+
+            Box(
+                modifier = Modifier.zIndex(1f)
+
+            ) {
+                Image(
+                    modifier = Modifier.size(70.dp),
+                    painter = painterResource(id = R.drawable.personback),
+                    contentDescription = "person"
+                )
+            }
+
+            Box(
+                //contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .background(Color(0xFF5A5788), shape = RoundedCornerShape(16.dp))
+                    .width(300.dp)
+                    .padding(start = 32.dp, end = 32.dp, top = 48.dp, bottom = 16.dp)
+                    .zIndex(0f),
+
+
+
+                ) {
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LoginField(
+                        value = credentials.id,
+                        onChange = { data -> credentials = credentials.copy(id = data) }
+                    )
+                    PasswordField(
+                        value = credentials.pwd,
+                        onChange = { data -> credentials = credentials.copy(pwd = data) },
+                        submit = { checkCredentials(credentials, context) }
+                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        rememberInfo(
+                            label = "로그인 유지",
+                            onCheckChanged = {credentials = credentials.copy(remember = credentials.remember)},
+                            isChecked = credentials.remember
+                        )
+                        Text(
+                            text = "회원가입 하기",
+                            fontSize = 12.sp,
+                            color = Color.White,
+                            modifier = Modifier.clickable {
+                                // 클릭시 회원가입 화면으로 넘어가기
+                            }
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(
+                        onClick = { checkCredentials(credentials, context) },
+                        enabled = credentials.isNotEmpty(),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(200.dp)
+                    ) {
+                        Text("로그인")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+fun checkCredentials(creds: Credentials, context: Context) {
+
+    if(creds.isNotEmpty() && creds.id == "admin") {
+        context.startActivity(Intent(context, MainActivity::class.java))
+        (context as Activity).finish()
+    } else {
+        Toast.makeText(context, "아이디 또는 비밀번호가 옳지 않습니다", Toast.LENGTH_SHORT).show()
+    }
+}
+
+data class Credentials(var id : String = "", var pwd : String = "", var remember : Boolean = false) {
+    fun isNotEmpty() : Boolean {
+        return id.isNotEmpty() && pwd.isNotEmpty()
+    }
+}
+
+
+@Composable
+fun rememberInfo(
+    label : String,
+    onCheckChanged : () -> Unit,
+    isChecked : Boolean
+) {
+    var checkedState by remember { mutableStateOf(isChecked) }
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable(
+                onClick = onCheckChanged
+            )
+
+    ) {
+        Checkbox(
+            checked = checkedState,
+            onCheckedChange = {
+                checkedState = it
+                onCheckChanged()
+            },
+            modifier = Modifier.scale(0.8f),
+            colors = CheckboxDefaults.colors(
+                //checkmarkColor = Color.Red, // 체크된 상태의 색
+                //checkedColor = Color.Blue, // 체크된 상태의 배경 색
+                //uncheckedColor = Color.Gray, // 체크되지 않은 상태의 배경 색
+            )
+        )
+        Spacer(modifier = Modifier.size(6.dp))
+        Text(text = label, fontSize = 12.sp, color = Color.White)
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginField(
+    value : String,
+    onChange : (String) -> Unit,
+    placeholder : String = "아이디"
+) {
+
+    val focusManager = LocalFocusManager.current
+
+    val leadingIcon = @Composable {
+        Icon(
+            Icons.Default.Person,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        //modifier = Modifier,
+        leadingIcon = leadingIcon,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(
+            onNext = { focusManager.moveFocus(FocusDirection.Down) }
+        ),
+        placeholder = { Text(placeholder) },
+        singleLine = true,
+        visualTransformation = VisualTransformation.None
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordField(
+    value : String,
+    onChange : (String) -> Unit,
+    placeholder : String = "비밀번호",
+    submit : () -> Unit
+) {
+
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    val leadingIcon = @Composable {
+        Icon(
+            Icons.Default.Lock,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+    val trailingIcon = @Composable {
+        IconButton(
+            onClick = { passwordVisible = !passwordVisible },
+        ) {
+            Icon(
+                if(passwordVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+
+
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        //modifier = Modifier,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(
+            onDone = { submit() }
+        ),
+        placeholder = { Text(placeholder) },
+        singleLine = true,
+        visualTransformation = if(passwordVisible) VisualTransformation.None else PasswordVisualTransformation()
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginActivityScreenPreview() {
+    MoodtrakerTheme {
+        LoginScreen()
+    }
+}
--- app/src/main/java/com/example/moodtraker/MainActivity.kt
+++ app/src/main/java/com/example/moodtraker/MainActivity.kt
@@ -1,64 +1,38 @@
 package com.example.moodtraker
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Leaderboard
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.DividerDefaults.color
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarDefaults
-import androidx.compose.material3.SnackbarDefaults.color
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -70,11 +44,6 @@ import com.example.moodtraker.screen.CalendarScreen
 import com.example.moodtraker.screen.GraphScreen
 import com.example.moodtraker.screen.LogScreen
 import com.example.moodtraker.ui.theme.MoodtrakerTheme
-import com.google.android.gms.games.leaderboard.Leaderboard
-import kotlinx.coroutines.delay
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -225,19 +194,6 @@ fun ImageLogo(){
         }
     }
 }
-
-@Composable
-fun LoginScreen(){
-
-}
-
-
-
-
-
-
-
-
 
 
 //@Composable
