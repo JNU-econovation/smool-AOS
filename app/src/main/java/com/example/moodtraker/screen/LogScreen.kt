9bbe165846cb4aebaa28ee5f7d48eaa29a94e0de
seoyun <xeonj@naver.com>
2024-01-23 21:26:48 +0900

    design: 로그 화면 emotionBox 텍스트 간격 상대값으로 수정

--- app/src/main/java/com/example/moodtraker/screen/LogScreen.kt
+++ app/src/main/java/com/example/moodtraker/screen/LogScreen.kt
@@ -708,31 +708,31 @@ fun emotionBox(happiness : MutableState<Int>, gloom : MutableState<Int>, anxiety
                     //horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     //
-                    //Spacer(modifier = Modifier.weight(0.8f))
+                    Spacer(modifier = Modifier.weight(0.8f))
                     Text(
                         text = happiness.value.toString(),
                         color = Color.White,
-                        modifier = Modifier.padding(start = 56.dp)
+                        modifier = Modifier.weight(1f)
                     )
                     Text(
                         gloom.value.toString(),
                         color = Color.White,
-                        modifier = Modifier.padding(start = 40.dp)
+                        modifier = Modifier.weight(1f)
                     )
                     Text(
                         anxiety.value.toString(),
                         color = Color.White,
-                        modifier = Modifier.padding(start = 42.dp)
+                        modifier = Modifier.weight(1f)
                     )
                     Text(
                         stress.value.toString(),
                         color = Color.White,
-                        modifier = Modifier.padding(start = 54.dp)
+                        modifier = Modifier.weight(1f)
                     )
                     Text(
                         sleep.value.toString(),
                         color = Color.White,
-                        modifier = Modifier.padding(start = 60.dp)
+                        modifier = Modifier.weight(1f)
                     )
                 }
             }
